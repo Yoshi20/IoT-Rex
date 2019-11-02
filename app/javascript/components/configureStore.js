0fb9e8a4efbe5ae64135df25dbade0792a011608
Jascha Haldemann <jh@oxon.ch>
2019-11-02 20:59:36 +0100

    redux-thunk & redux-devtools-extension

--- app/javascript/components/configureStore.js
+++ app/javascript/components/configureStore.js
@@ -1,4 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+import thunk from "redux-thunk";
+
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {
   things: [
@@ -16,12 +20,22 @@ const initialState = {
 function rootReducer(state, action) {
   console.log(action.type);
   switch (action.type) {
+    case "GET_THINGS_SUCCESS":
+      return { things: action.json.things };
     default:
       return state
   }
 }
 
 export default function configureStore() {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(
+        thunk,
+      )
+    )
+  );
   return store;
 }
