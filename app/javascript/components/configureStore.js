f7d9c8450fc4829093b227687e483b07d526c22c
Jascha Haldemann <jh@oxon.ch>
2019-11-03 16:45:10 +0100

    redux-devtools-extension/developmentOnly

--- app/javascript/components/configureStore.js
+++ app/javascript/components/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 
 import thunk from "redux-thunk";
 
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const initialState = {
   things: [
