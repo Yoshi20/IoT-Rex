0b50e008b62220570581f7698937e528f75d32b9
Jascha Haldemann <jh@oxon.ch>
2019-11-03 16:04:05 +0100

    user_group has_many events

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,5 +1,6 @@
 class UserGroup < ApplicationRecord
   has_many :devices, dependent: :destroy
+  has_many :events, dependent: :destroy
   has_many :event_templates, dependent: :destroy
   has_many :event_template_lists, dependent: :destroy
   has_many :users, dependent: :destroy
