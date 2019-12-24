0d4392a695c6966c0a9e2ad9d4c70c96d9f05a74
Jascha Haldemann <jh@oxon.ch>
2019-12-24 18:34:38 +0100

    events.acknowledged

--- db/migrate/20191214124200_create_events.rb
+++ db/migrate/20191214124200_create_events.rb
@@ -3,6 +3,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.string :text
       t.string :data
+      t.boolean :acknowledged, default: false
       t.datetime :timeouts_at
 
       t.timestamps
