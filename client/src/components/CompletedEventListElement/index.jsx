d29d38d4fa6cd09386bdaee50bf0a9783e004072
Jascha Haldemann <jh@oxon.ch>
2020-01-08 16:34:01 +0100

    order desc when is_completed

--- client/src/components/CompletedEventListElement/index.jsx
+++ client/src/components/CompletedEventListElement/index.jsx
@@ -4,9 +4,7 @@ import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import AlarmIcon from '@material-ui/icons/AccessAlarm';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import SubDirArrowRightIcon from '@material-ui/icons/SubdirectoryArrowRight';
+import SubDirArrowLeftIcon from '@material-ui/icons/SubdirectoryArrowLeft';
 // import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
 // import HourglassFullIcon from '@material-ui/icons/HourglassFull';
 
@@ -21,7 +19,7 @@ export default function CompletedEventListElement({ text, time, acknowledged, ac
   let date_str = date.toLocaleString();
   let hhmm = date_str.slice(date_str.length - 8, date_str.length - 3);
   let ack_at_str = (new Date(acknowledged_at)).toLocaleString();
-  let ack_hhmm = ack_at_str.slice(ack_at_str.length - 8, ack_at_str.length - 3);
+  // let ack_hhmm = ack_at_str.slice(ack_at_str.length - 8, ack_at_str.length - 3);
   let max_t = (timeout - date)/1000
   let dt = 0
   if (acknowledged) dt = Math.round((timeout - acknowledged_at)/1000)
@@ -42,17 +40,14 @@ export default function CompletedEventListElement({ text, time, acknowledged, ac
       {(acknowledged && !timeouted) && (
         <div className={styles.completedEventListElement__icon}><span><CheckCircleOutlineIcon /></span></div>
       )}
-      {(!acknowledged && !timeouted) && (
-        <div className={styles.completedEventListElement__icon}><span><ArrowForwardIcon /></span></div>
-      )}
       {[...Array(level-1)].map((_, i) => (
-        <div key={i} className={styles.completedEventListElement__icon}><span><SubDirArrowRightIcon /></span></div>
+        <div key={i} className={styles.completedEventListElement__icon}><span><SubDirArrowLeftIcon className={styles.rotated} /></span></div>
       ))}
       {/* Event content */}
       <div className={styles.completedEventListElement__time}>{hhmm}</div>
       <div className={styles.completedEventListElement__text}>{text}</div>
       {(acknowledged) && (
-        <div className={styles.completedEventListElement__name}>(von {acknowledged_by} um {ack_hhmm})</div>
+        <div className={styles.completedEventListElement__name}>(von {acknowledged_by.split(' ')[0]} {acknowledged_by.split(' ')[1][0]}. am {ack_at_str.slice(0, ack_at_str.length - 3)})</div>
       )}
       {!isNaN(dt) && (
         <div className={styles.completedEventListElement__timeout}>
@@ -70,9 +65,6 @@ export default function CompletedEventListElement({ text, time, acknowledged, ac
       )}
       {(acknowledged && !timeouted) && (
         <div className={styles.completedEventListElement__icon}><span><CheckCircleOutlineIcon /></span></div>
-      )}
-      {(!acknowledged && !timeouted) && (
-        <div className={styles.completedEventListElement__icon}><span><ArrowBackIcon /></span></div>
       )}
     </div>
   );
